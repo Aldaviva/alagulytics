13a4d22a36bd97b30d816e346b8980e5f2d1f60f
Ben Hutchison <ben@bluejeans.com>
2014-01-10 13:33:14 -0800

    activityState graph styling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,7 @@ var endTime = +new Date();
 
 var API_ROOT = '/cgi-bin/';
 
-//main();
+main();
 
 function main(){
 	renderLocation();
@@ -31,7 +31,7 @@ function renderLocation(){
 				$.getJSON(API_ROOT+'/events/geofence?limit=1&sort=-time'+getDateFilter())
 					.done(function(events){
 						var geofence = events[0].value;
-						locationValueEl.text(geofence;)
+						locationValueEl.text(geofence);
 					});
 			}
 		});
@@ -60,14 +60,23 @@ function renderStepsGraph(){
 }
 
 function renderActivityGraph(){
+	var activityEnum = {
+		"-1": "unknown",
+		"0": "stopped",
+		"1": "walking",
+		"2": "running",
+		"3": "driving",
+		"4": "unused"
+	};
 	var graphEl = $('.activityState .graph');
+	graphEl.empty();
 
 	$.getJSON(API_ROOT+'/events/activityState?sort=+time&'+getDateFilter())
 		.done(function(events){
 			var previousEventEl = null;
 			_.each(events, function(event){
 				var pixelOffset = (event.time - startTime)/(endTime - startTime)*graphEl.width();
-				var eventEl = $('<div>').css({
+				var eventEl = $('<div>', { class: activityEnum[String(event.value)] }).css({
 					left: pixelOffset
 				});
 
